503a952346deccea2abfecaebb5ce71346409662
naajiya-ebrahim <159777734+naajiya-ebrahim@users.noreply.github.com>
2024-06-18 09:19:40 +0200

    Add files via upload

---
+++ java project - email app/src/emailApp/EmailApp.java
@@ -0,0 +1,9 @@
+package emailApp;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        email em1 = new email("Sally","Jones");
+        System.out.print(em1.showInfo());
+    }
+}
